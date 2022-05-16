68831dde8cacf86540ae8d7381dc3fd24dc6f26b
Lena Cabrera <lena.m.cabrera@gmail.com>
2022-05-16 13:06:41 +0200

    Add pivoting script for IWSLT

---
+++ recipes/zero-shot/pred.iwslt.pivot.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+source ./recipes/zero-shot/config.sh
+set -eu
+
+
+export MOSES=~/opt/mosesdecoder
+export DATA_DIR=~/export/data2/lcabrera/data/iwslt17_multiway/test/tok # path to tokenized test data
+export BASEDIR=~/export/data2/lcabrera	# path to model & orig data
+export name=$1 	# model name
+
+LAN="it nl ro"
+pl="en"
+
+mkdir $BASEDIR/data/$name -p
+
+# 1. source -> EN
+for sl in $LAN; do
+for tl in $LAN; do
+
+if [[ ! "$sl" == "$tl" ]]; then
+
+pred_src=$DATA_DIR/tst2017$sl-$pl.real.s
+
+echo $pred_src
+
+p_out=$BASEDIR/data/$name/$sl-$pl.pred
+out=$BASEDIR/data/$name/$pl-$tl.pred
+
+echo "Input:" $pred_src 
+echo "Pivot output: " $p_out
+echo "Output: " $out
+
+bos='#'${pl^^}
+
+python3 -u $NMTDIR/translate.py -gpu $GPU \
+        -model $BASEDIR/model/baseline/iwslt.pt \
+        -src $pred_src \
+        -batch_size 128 -verbose \
+        -beam_size 4 -alpha 1.0 \
+        -normalize \
+        -output $p_out \
+        -fast_translate \
+        -src_lang $sl \
+        -tgt_lang $pl \
+        -bos_token $bos
+
+# postprocess output
+sed -e "s/@@ //g" $p_out  | sed -e "s/@@$//g" | sed -e "s/&apos;/'/g" -e 's/&#124;/|/g' -e "s/&amp;/&/g" -e 's/&lt;/>/g' -e 's/&gt;/>/g' -e 's/&quot;/"/g' -e 's/&#91;/[/g' -e 's/&#93;/]/g' -e 's/ - /-/g' | sed -e "s/ '/'/g" | sed -e "s/ '/'/g" | sed -e "s/%- / -/g" | sed -e "s/ -%/- /g" | perl -nle 'print ucfirst' > $p_out.tok
+
+$MOSESDIR/scripts/tokenizer/detokenizer.perl -l $pl < $p_out.tok > $p_out.detok
+$MOSESDIR/scripts/recaser/detruecase.perl < $p_out.detok > $p_out.pt
+
+# 2. EN -> target
+bos='#'${tl^^}
+
+python3 -u $NMTDIR/translate.py -gpu $GPU \
+       -model $BASEDIR/model/baseline/iwslt.pt \
+       -src $p_out \
+       -batch_size 128 -verbose \
+       -beam_size 4 -alpha 1.0 \
+       -normalize \
+       -output $out \
+       -fast_translate \
+       -src_lang $pl \
+       -tgt_lang $tl \
+       -bos_token $bos
+
+        # postprocess output
+        sed -e "s/@@ //g" $out  | sed -e "s/@@$//g" | sed -e "s/&apos;/'/g" -e 's/&#124;/|/g' -e "s/&amp;/&/g" -e 's/&lt;/>/g' -e 's/&gt;/>/g' -e 's/&quot;/"/g' -e 's/&#91;/[/g' -e 's/&#93;/]/g' -e 's/ - /-/g' | sed -e "s/ '/'/g" | sed -e "s/ '/'/g" | sed -e "s/%- / -/g" | sed -e "s/ -%/- /g" | perl -nle 'print ucfirst' > $out.tok
+
+        $MOSESDIR/scripts/tokenizer/detokenizer.perl -l $tl < $out.tok > $out.detok
+        $MOSESDIR/scripts/recaser/detruecase.perl < $out.detok > $out.pt
+	
+        echo '===========================================' $sl $pl $tl
+        # Evaluate against original reference  
+        cat $out.pt | sacrebleu $BASEDIR/data/iwslt17_multiway/test/orig/tst2017$tl-$sl.$tl > $BASEDIR/data/$name/$sl-$tl.test.res
+        cat $BASEDIR/data/$name/$sl-$tl.test.res
+            
+fi
+
+done
+done
