fa6fdeef49df6d243e5d619b02cc4d7a7cd15a2c
Lena Cabrera <lena.m.cabrera@gmail.com>
2022-08-05 15:24:09 +0200

    Create more training data sets from MuST-C corpus

--- utils/create_twoway_corpora_from_multiway.sh
+++ utils/create_twoway_corpora_from_multiway.sh
@@ -15,10 +15,15 @@ mkdir -p $DATADIR/mustc/prepro_20000_subwordnmt/multiwayIT
 mkdir -p $DATADIR/mustc/prepro_20000_subwordnmt/multiwayIT/train
 mkdir -p $DATADIR/mustc/prepro_20000_subwordnmt/multiwayIT/valid
 
-out_path_es_it_fr=$DATADIR/mustc/prepro_20000_subwordnmt/multiway_es_it_fr
-mkdir -p $DATADIR/mustc/prepro_20000_subwordnmt/multiway_es_it_fr
-mkdir -p $DATADIR/mustc/prepro_20000_subwordnmt/multiway_es_it_fr/train
-mkdir -p $DATADIR/mustc/prepro_20000_subwordnmt/multiway_es_it_fr/valid
+out_path_es_it_fr=$DATADIR/mustc/prepro_20000_subwordnmt/multiwayESFRIT
+mkdir -p $DATADIR/mustc/prepro_20000_subwordnmt/multiwayESFRIT
+mkdir -p $DATADIR/mustc/prepro_20000_subwordnmt/multiwayESFRIT/train
+mkdir -p $DATADIR/mustc/prepro_20000_subwordnmt/multiwayESFRIT/valid
+
+out_path_DE=$DATADIR/mustc/prepro_20000_subwordnmt/multiwayDE
+mkdir -p $DATADIR/mustc/prepro_20000_subwordnmt/multiwayDE
+mkdir -p $DATADIR/mustc/prepro_20000_subwordnmt/multiwayDE/train
+mkdir -p $DATADIR/mustc/prepro_20000_subwordnmt/multiwayDE/valid
 
  for set in train valid; do
 
@@ -72,15 +77,35 @@ mkdir -p $DATADIR/mustc/prepro_20000_subwordnmt/multiway_es_it_fr/valid
      for f in $path/$set/*-it*\.t; do
          cp -f $f $out_path_IT/$set
      done
+
+    # de
+     for f in $path/$set/de-*\.s; do
+         cp -f $f $out_path_DE/$set
+     done
+
+     for f in $path/$set/*-de*\.s; do
+         cp -f $f $out_path_DE/$set
+     done
+
+     for f in $path/$set/de-*\.t; do
+         cp -f $f $out_path_DE/$set
+     done
+
+     for f in $path/$set/*-de*\.t; do
+         cp -f $f $out_path_DE/$set
+     done
  done
 
 partial_path=$DATADIR/mustc/prepro_20000_subwordnmt
-for lan in multiwayES multiwayFR multiwayIT; do
+for lan in multiwayES multiwayFR multiwayIT multiwayDE; do
     for set in train valid; do
         for f in $partial_path/$lan/$set/*; do
-            echo $lan $set
-            echo $f
-            cp -f $f $out_path_es_it_fr/$set
+            # echo $lan $set
+            # echo $f
+            if [[ $lan != multiwayDE ]]; then
+                echo $f
+                cp -f $f $out_path_es_it_fr/$set
+            fi
         done
     done
 done
